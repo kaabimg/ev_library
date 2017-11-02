ff74c262b8513c37b6a6e24ac2cad80695b6a71f
Mohamed-Ghaith Kaabi <mohamedghaith.kaabi@gmail.com>
2017-11-02 23:08:10 +0100

    ml dev

---
+++ bin/ml.cpp
@@ -0,0 +1,10 @@
+
+#include <ev/ml/neural_network.hpp>
+#include <ev/ml/algorithm.hpp>
+#include <ev/core/logging_helpers.hpp>
+#include <ev/core/overloaded.hpp>
+
+int main()
+{
+    ev::ml::neural_network network;
+}
--- ev/ml/neural_network.hpp
+++ ev/ml/neural_network.hpp
@@ -1,15 +1,29 @@
 #pragma once
-
+#include <cmath>
+#include <vector>
 namespace ev {
 namespace ml {
 
+
+struct input_neuron {
+private:
+    double _value = 0.0;
+};
+
 struct neuron {
+private:
+    std::vector<double> _weights;
+    double _value = 0.0;
 };
 
 struct layer {
+private:
+    std::vector<neuron> _neurons;
 };
 
 struct neural_network {
+private:
+    std::vector<layer> _layers;
 };
 }
 }
